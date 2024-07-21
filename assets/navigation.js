7aadfe1c91da6a85953077b69c0806c991727c16
nehamahin <syedaa.business@gmail.com>
2024-07-21 13:25:20 +0530

    Update navigation.js
    refactored to fix the auto open-close behavior

--- assets/navigation.js
+++ assets/navigation.js
@@ -3,46 +3,38 @@ const closeNav = document.getElementById("mobile-nav__close");
 const menuNavBtn = document.getElementById("mobile-menu");
 const allMobileNavItems = document.querySelectorAll(".mobile-nav__menu--item");
 
+const openMobileNav = () => {
+    mobileNav.classList.add("active");
+    mobileNav.style.transform = "translateX(0%)";
+};
+
 const closeMobileNav = () => {
     mobileNav.style.transform = "translateX(100%)";
     setTimeout(() => {
         mobileNav.classList.remove("active");
-    }, 1000);
+    }, 300);
 };
 
-if (mobileNav) {
-    closeNav.addEventListener("click", (e) => {
-        if (
-            e.target.parentNode.classList.contains("mobile-nav__close--svg") ||
-            e.target.parentNode.id === "mobile-nav__close"
-        ) {
-            closeMobileNav();
-        }
+if (menuNavBtn) {
+    menuNavBtn.addEventListener("click", () => {
+        openMobileNav();
     });
 }
 
-if (menuNavBtn) {
-    menuNavBtn.addEventListener("click", (e) => {
-        if (
-            e.target.parentNode.classList.contains("mobile-menu__svg") ||
-            e.target.parentNode.id === "mobile-menu"
-        ) {
-            mobileNav.classList.add("active");
-            setTimeout(() => {
-                mobileNav.style.transform = "translateX(0%)";
-            }, 100);
-        }
+if (closeNav) {
+    closeNav.addEventListener("click", () => {
+        closeMobileNav();
     });
 }
 
 allMobileNavItems.forEach((navlink) => {
-    navlink.addEventListener("click", (e) => {
-        if (
-            e.target.parentNode.classList.contains("mobile-nav__menu--item") ||
-            e.target.parentNode.classList.contains("item")
-        ) {
-            closeMobileNav();
-        }
+    navlink.addEventListener("click", () => {
+        closeMobileNav();
     });
 });
-/******** MOBILE NAV FUNCTIONALITY ENDS HERE ******* */
+
+document.addEventListener("click", (e) => {
+    if (!mobileNav.contains(e.target) && !menuNavBtn.contains(e.target)) {
+        closeMobileNav();
+    }
+});
