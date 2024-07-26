30f274dc806a206cdac0cf9c47f0e6e14b3a84ea
nehamahin <syedaa.business@gmail.com>
2024-07-26 21:34:34 +0530

    refactored mobile menu

--- assets/navigation.js
+++ assets/navigation.js
@@ -3,38 +3,21 @@ const closeNav = document.getElementById("mobile-nav__close");
 const menuNavBtn = document.getElementById("mobile-menu");
 const allMobileNavItems = document.querySelectorAll(".mobile-nav__menu--item");
 
-const openMobileNav = () => {
-    mobileNav.classList.add("active");
-    mobileNav.style.transform = "translateX(0%)";
+const toggleMobileNav = () => {
+    mobileNav.classList.toggle("active");
 };
 
-const closeMobileNav = () => {
-    mobileNav.style.transform = "translateX(100%)";
-    setTimeout(() => {
-        mobileNav.classList.remove("active");
-    }, 300);
-};
-
-if (menuNavBtn) {
-    menuNavBtn.addEventListener("click", () => {
-        openMobileNav();
-    });
-}
-
-if (closeNav) {
-    closeNav.addEventListener("click", () => {
-        closeMobileNav();
-    });
+if (menuNavBtn || closeNav) {
+    menuNavBtn.addEventListener("click", () => toggleMobileNav());
+    closeNav.addEventListener("click", () => toggleMobileNav());
 }
 
 allMobileNavItems.forEach((navlink) => {
-    navlink.addEventListener("click", () => {
-        closeMobileNav();
-    });
+    navlink.addEventListener("click", () => toggleMobileNav());
 });
 
 document.addEventListener("click", (e) => {
     if (!mobileNav.contains(e.target) && !menuNavBtn.contains(e.target)) {
-        closeMobileNav();
+        toggleMobileNav();
     }
 });
